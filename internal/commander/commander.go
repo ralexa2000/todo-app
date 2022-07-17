fc08c81267344b7ab8dc6ede0c623399947e4719
Alexandra Ryabinina <aoryabinina@gmail.com>
2022-07-17 09:04:31 +0300

    delete command

--- internal/commander/commander.go
+++ internal/commander/commander.go
@@ -16,6 +16,7 @@ const (
 	listCmd   = "list"
 	addCmd    = "add"
 	updateCmd = "update"
+	deleteCmd = "delete"
 )
 
 type Commander struct {
@@ -56,6 +57,8 @@ func (c *Commander) Run() error {
 				msg.Text = addFunc(userName, update.Message.Text)
 			case updateCmd:
 				msg.Text = updateFunc(userName, update.Message.Text)
+			case deleteCmd:
+				msg.Text = deleteFunc(userName, update.Message.Text)
 			default:
 				msg.Text = UnknownCommand.Error()
 			}
@@ -77,7 +80,11 @@ func listFunc(userName string) string {
 	for _, t := range data {
 		res = append(res, t.String())
 	}
-	return strings.Join(res, "\n")
+	outString := strings.Join(res, "\n")
+	if outString == "" {
+		outString = "no tasks, add some with /add command"
+	}
+	return outString
 }
 
 func addFunc(userName, inputString string) string {
@@ -119,6 +126,29 @@ func updateFunc(userName, inputString string) string {
 	if err = t.SetDueDate(matched[2]); err != nil {
 		return err.Error()
 	}
-	err = storage.Update(t)
+	if err = storage.Update(t); err != nil {
+		return err.Error()
+	}
 	return "task updated"
+}
+
+func deleteFunc(userName, inputString string) string {
+	re := regexp.MustCompile(`^/delete (\d+)$`)
+	matched := re.FindStringSubmatch(inputString)
+	log.Printf("%q\n", matched)
+	if len(matched) != 2 {
+		return BadArgument.Error()
+	}
+	id, _ := strconv.ParseUint(matched[1], 10, 64)
+	t, err := storage.GetById(uint(id))
+	if err != nil {
+		return err.Error()
+	}
+	if t.GetUser() != userName {
+		return NoAccess.Error()
+	}
+	if err = storage.Delete(t); err != nil {
+		return err.Error()
+	}
+	return "task deleted"
 }
